82d1ff8b880f26ae7654977dec90d598d638a915
Peter Furey <prfurey@gmail.com>
2015-09-23 14:50:34 -0600

    Create first version of site

---
+++ application.js
@@ -0,0 +1,5 @@
+
+//$('.parallax-window').text("MOO");
+
+// $('.parallax-window').parallax({imageSrc: 'images/Asphalt-Stone-Background-73-mod.png'});
+$('.parallax-window').parallax({imageSrc: 'images/grunge-blue-background.jpg'});
