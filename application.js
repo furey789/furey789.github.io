d534ef2c420772de2ac3e02a57d747077e1b781c
Peter Furey <prfurey@gmail.com>
2016-11-15 10:52:23 -0700

    Add new background image

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 
 //$('.parallax-window').text("MOO");
 
-$('.parallax-window').parallax({imageSrc: 'images/asphalt-stone-background.png'});
+//$('.parallax-window').parallax({imageSrc: 'images/asphalt-stone-background.png'});
 // $('.parallax-window').parallax({imageSrc: 'images/grunge-blue-background.jpg'});
 //$('.parallax-window').parallax({imageSrc: 'images/forest-bw-v2.png'});
+$('.parallax-window').parallax({imageSrc: 'images/enLineaMD_2016.11.15_1.jpg'});
